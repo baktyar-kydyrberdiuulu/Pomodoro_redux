9ef52b6be74cb31d8dc6b0569a08809f618dfc14
Kydyrberdi uulu Baktyiar <kydyrberdiuulubaktyar@gmail.com>
2021-07-01 00:53:57 +0600

    Updates

---
+++ precache-manifest.166fb4d8b399d2c7dfb2e56d2e44baf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4496daf60e7d15b8261bb6528ef9c9c7",
+    "url": "/Pomodoro_redux/index.html"
+  },
+  {
+    "revision": "6e8d63a53ac79d95f8c2",
+    "url": "/Pomodoro_redux/static/css/main.792e9a24.chunk.css"
+  },
+  {
+    "revision": "fca65d55c134afb62e2e",
+    "url": "/Pomodoro_redux/static/js/2.e80bd427.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/Pomodoro_redux/static/js/2.e80bd427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8d63a53ac79d95f8c2",
+    "url": "/Pomodoro_redux/static/js/main.bdaaa134.chunk.js"
+  },
+  {
+    "revision": "39ffc4c6129828855e79",
+    "url": "/Pomodoro_redux/static/js/runtime-main.672b8162.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro_redux/static/media/bell.525320df.flac"
+  }
+]);
